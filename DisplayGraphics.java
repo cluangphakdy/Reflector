3cc9b9ddb8d9ed44383c5b42915b1f8e7131f859
cluangphakdy <CoolColin110@gmail.com>
2025-02-18 17:55:37 -0500

    add

---
+++ DisplayGraphics.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class DisplayGraphics {
+    public void init() {
+        JFrame frame = new JFrame();
+        frame.setBounds(0, 0, 1900, 1500);
+        frame.setVisible(true);
+    }
+}
---
+++ Floor.java
@@ -0,0 +1,34 @@
+public class Floor {
+    private static String[][] board = new String[20][10];
+    /**
+   [-"empty" = empty places of board, no mirror, no laser, maybe background
+    -"lazer" = lazer
+    -"mirror" = mirror, reflects the lazer, what player is controlling
+    -"mirror90" = rotation value of the mirror, ex: mirror rotated 90 degrees]
+    Bracket text is part of board
+     */
+
+    public static void main(String[] args){
+        FillBoard();
+        PrintBoard();
+        DisplayGraphics screen = new DisplayGraphics();
+        screen.init();
+    }
+    private static void FillBoard() {
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 20; j++){
+                board[j][i] = "empty";
+
+            }
+        }
+    }
+
+    private static void PrintBoard() {
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 20; j++){
+                System.out.print(board[j][i]);
+            }
+            System.out.println();
+        }
+    }
+}
