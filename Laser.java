f6e155c926265b6330abf65b3cc495409ab4585c
cluangphakdy <CoolColin110@gmail.com>
2025-04-23 17:51:17 -0400

    Create Laser.java

---
+++ Laser.java
@@ -0,0 +1,11 @@
+public class Laser {
+}
+
+
+
+
+//TODO:45 or 225 degree mirrors move the laser diagonally (up and left or right)
+//TODO:90 or 270 degree mirrors move the laser up
+//TODO:180 degree mirrors move the mirror left and right
+//TODO: 0 or 360 degree mirrors move the lasers back in the direction it came from (regardless of reflections)
+//TODO:laser is made up of tiny cubes and is asserted an angle after each mirror it comes in contact with
