4579382c8ff1f922036456c4f1f39d9c807e2304
cluangphakdy <CoolColin110@gmail.com>
2025-04-30 17:51:08 -0400

    Update Mirror.java

--- Mirror.java
+++ Mirror.java
@@ -9,7 +9,12 @@ public class Mirror {
     private int rotdegree;
     private BufferedImage img; //used to be static
 
+    private int mirrorwidth;
+    private int mirrorheight;
+
     public Mirror(int px, int py, int deg) throws IOException {
+        this.mirrorwidth = 50;
+        this.mirrorheight = 75;
         this.x = px;
         this.y = py;
         this.rotdegree = deg;
@@ -18,7 +23,7 @@ public class Mirror {
 
     public void drawMirror(Graphics g, int x, int y) throws IOException {
         //BufferedImage mirrorimg = ImageIO.read(img);
-        g.drawImage(this.img, x, y, 50, 75, null);
+        g.drawImage(this.img, x, y, mirrorwidth, mirrorheight, null);
     }
 
     public void prntmirrorvalues() {
@@ -28,9 +33,20 @@ public class Mirror {
     public void mirrorRotate() throws IOException {
         rotdegree+= 45;
         if (rotdegree >= 360) {
-            rotdegree-= rotdegree;
+            rotdegree=0;
+            mirrorwidth=50;
+            mirrorheight=75;
         }
-        System.out.println(rotdegree);
+        if (rotdegree == 90 || rotdegree == 270) {
+            mirrorwidth = 100;
+
+        }
+        else {
+            mirrorwidth =50;
+            mirrorheight=75;
+        }
+
+        System.out.println("rotation degree: "+rotdegree);
         this.img = ImageIO.read(new File("src/Mirror" + rotdegree + ".png"));
     }
 
