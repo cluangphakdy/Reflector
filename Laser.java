7fe8ccc9ad255976d2dbd47ff61aa0997cfd2aea
cluangphakdy <CoolColin110@gmail.com>
2025-05-14 18:50:44 -0400

    Update Laser.java
    dsaasccsacas

--- Laser.java
+++ Laser.java
@@ -2,12 +2,43 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Laser {
-    ArrayList<Point> beam;
+    static Color red = new Color(255, 0, 0, 255);
+    static ArrayList<int[]> beam = new ArrayList<int[]>();
     public Laser() {
-        beam=new ArrayList<>();
+
+
+    }
+
+    public static void addPoints(){
+        beam = new ArrayList<int[]>();
+        beam.add(new int[]{400, 400});
+        beam.add(new int[]{600, 600});
+        beam.add(new int[]{700, 900});
+
+    }
+    static public void drawLaser(Graphics g){
+        g.setColor(Color.red);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(3));
+        for (int i = 1; i < beam.size(); i++){
+            int[] point1 = beam.get(i-1);
+            int[] point2 = beam.get(i);
+            g2.drawLine(point1[0], point1[1], point2[0], point2[1]);
+
+        }
+        System.out.println(beam.size());
+
     }
 
 }
+
+
+
+
+
+
+
+
 
 
 
